9b223148ef3551d56fff76f8dd4acee81809f3ee
youzhenlim <limyouzhen@gmail.com>
2023-06-11 17:01:12 +0800

    initialize cookie/sessions

--- index.js
+++ index.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cookieSession = require('cookie-session');
 const usersRepo = require('./repositories/users');
 
 const app = express();
 
 //For app.use() allows all our different route handlers to have access to the middleware function
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(    // Note: The keys can be any random string
+    cookieSession({keys: ['gfkdgkfwedsfsewwervcbcveswqd']
+    })
+);
 
 //Req stands for incoming request from a browser to web server
 //Res stands for the response that the web server sends back to the browser
-app.get('/', (req, res) =>{
+app.get('/signup', (req, res) =>{
     res.send(`
         <div>
+            Your ID is : ${req.session.userId}
             <form method="POST">
                 <input name="email" placeholder="email">
                 <input name="password" placeholder="password">
@@ -22,7 +28,7 @@ app.get('/', (req, res) =>{
     `);
 });
 
-app.post('/', async (req, res) => {
+app.post('/signup', async (req, res) => {
     //Destructuring
     const {email, password, passwordConfirmation} = req.body;
 
@@ -36,8 +42,11 @@ app.post('/', async (req, res) => {
         return res.send('Passwords must match');
     }
 
-    res.send('Account created!')
+    const user = await usersRepo.create({email, password});
 
+    req.session.userId = user.id;
+
+    res.send('Account Created!!!');
 });
 
 //Listen for incoming network requests on specified port 
