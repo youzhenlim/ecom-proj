6e29045c56e44cb84afae0e7f0a8423625b5a2e5
youzhenlim <limyouzhen@gmail.com>
2023-06-12 06:00:44 +0800

    extracted validators to separate file

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,6 +3,8 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
+const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+
 const router = express.Router();
 
 //Req stands for incoming request from a browser to web server
@@ -12,30 +14,9 @@ router.get('/signup', (req, res) =>{
 });
 
 router.post('/signup', [
-    check('email')
-    .trim()
-    .normalizeEmail()
-    .isEmail()
-    .withMessage('Must be a valid email')
-    .custom( async (email) => {
-        const existingUser = await usersRepo.getOneBy( {email: email});
-        if (existingUser){
-            throw new Error('Email in use');
-        }
-    }), 
-    check('password')
-    .trim()
-    .isLength({min:4, max:20})
-    .withMessage('Must be between 4 and 20 characters'), 
-    check('passwordConfirmation')
-    .trim()
-    .isLength({min:4, max:20})
-    .withMessage('Must be between 4 and 20 characters')
-    .custom( (passwordConfirmation,  { req } ) => {
-        if(passwordConfirmation !== req.body.password){
-            throw new Error('Passwords must match');
-        }
-    }),
+    requireEmail, 
+    requirePassword, 
+    requirePasswordConfirmation
     ], 
     async (req, res) => {
     const errors = validationResult(req);
