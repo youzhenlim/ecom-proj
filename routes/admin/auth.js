69f0f84e2c2357023ec468de4e93712756099f94
youzhenlim <limyouzhen@gmail.com>
2023-06-12 11:44:28 +0800

    added validation for signin

--- routes/admin/auth.js
+++ routes/admin/auth.js
@@ -3,7 +3,7 @@ const { check, validationResult } = require('express-validator');
 const usersRepo = require('../../repositories/users');
 const signupTemplate = require('../../views/admin/auth/signup');
 const signinTemplate = require('../../views/admin/auth/signin');
-const { requireEmail, requirePassword, requirePasswordConfirmation } = require('./validators');
+const { requireEmail, requirePassword, requirePasswordConfirmation, requireEmailExists, requireValidPasswordForUser } = require('./validators');
 
 const router = express.Router();
 
@@ -44,18 +44,18 @@ router.get('/signin', (req, res) => {
     res.send(signinTemplate());
 });
 
-router.post('/signin', async (req, res) => {
-    const { email, password } = req.body;
+router.post('/signin', [
+    requireEmailExists,
+    requireValidPasswordForUser
+    ], 
+    async (req, res) => {
+    const errors = validationResult(req);
+
+    console.log(errors);
+
+    const { email } = req.body;
     const user = await usersRepo.getOneBy({email});
-    if(!user){
-        return res.send('Email not found');
-    }
 
-    const validPassword = await usersRepo.comparePasswords(user.password, password);
-
-    if(!validPassword){
-        return res.send('Passwords do not match');
-    }
 
     req.session.userId = user.id;
 
