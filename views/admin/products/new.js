7f993d323e068ec61853b8a22e68f6387c37cacf
youzhenlim <limyouzhen@gmail.com>
2023-06-12 18:06:19 +0800

    added error messages to display for new products

--- views/admin/products/new.js
+++ views/admin/products/new.js
@@ -6,7 +6,9 @@ module.exports = ( {errors} ) => {
         content: `
             <form method="POST">
                 <input placeholder="Title" name="title">
+                ${getError(errors, 'title')}
                 <input placeholder="Price" name="price">
+                ${getError(errors, 'price')}
                 <input type="file" name="image">
                 <button>Submit</button>
             </form>
