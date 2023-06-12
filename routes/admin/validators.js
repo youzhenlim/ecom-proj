2469af522526d911da74b5396fcf5a6befefd031
youzhenlim <limyouzhen@gmail.com>
2023-06-12 17:50:29 +0800

    added image storage

--- routes/admin/validators.js
+++ routes/admin/validators.js
@@ -4,12 +4,14 @@ const usersRepo = require('../../repositories/users');
 module.exports = {
     requireTitle: check('title')
     .trim()
-    .isLength({min: 5, max: 40}),
+    .isLength({min: 5, max: 40})
+    .withMessage('Must be between 5 and 40 characters'),
 
     requirePrice: check('price')
     .trim()
     .toFloat()
-    .isFloat({min: 1,}),
+    .isFloat({min: 1})
+    .withMessage('Must have a value greater than 1'),
 
     requireEmail: check('email')
     .trim()
