9848b6632caac6f17587c9452e6433a971df6224
youzhenlim <limyouzhen@gmail.com>
2023-06-13 15:46:00 +0800

    added logic to show items in cart

--- routes/carts.js
+++ routes/carts.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const cartsRepo = require('../repositories/carts');
+const productsRepo = require('../repositories/products');
+const cardShowTemplate = require('../views/carts/show');
 
 const router = express.Router();
 
@@ -35,7 +37,24 @@ router.post('/cart/products', async (req,res) => {
 
     res.send('Product added to cart');
 });
+
 // Receive get request to show all items in cart
+router.get('/cart', async (req, res) => {
+    if(!req.session.cartId){
+        return res.redirect('/');
+    }
+
+    const cart = await cartsRepo.getOne(req.session.cartId);
+
+    for (let item of cart.items){
+        const product = await productsRepo.getOne(item.id);
+
+        item.product = product;
+    }
+
+    res.send(cartShowTemplate({ items: cart.items }));
+
+});
 
 // Receive post request to delete item from cart
 
