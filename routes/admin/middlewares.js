5076caca121f17154e6e2668b9f270f91f2c0f16
youzhenlim <limyouzhen@gmail.com>
2023-06-13 09:44:21 +0800

    added required authentication for admin product pages

--- routes/admin/middlewares.js
+++ routes/admin/middlewares.js
@@ -10,5 +10,13 @@ module.exports = {
 
             next();
         };
+    },
+
+    requireAuth(req, res, next){
+        if(!req.session.userId){
+            return res.redirect('/signin');
+        }
+
+        next();
     }
 };
