cb389391793ba383bcb03d9115c8e6b31449214c
youzhenlim <limyouzhen@gmail.com>
2023-06-13 10:06:36 +0800

    added url params on id and display edit form for each id

--- routes/admin/products.js
+++ routes/admin/products.js
@@ -5,6 +5,7 @@ const { handleErrors, requireAuth } = require('./middlewares');
 const productsRepo = require('../../repositories/products');
 const productsNewTemplate = require('../../views/admin/products/new');
 const productsIndexTemplate = require('../../views/admin/products/index');
+const productsEditTemplate = require('../../views/admin/products/edit');
 const {requireTitle, requirePrice} = require('./validators');
 
 const router = express.Router();
@@ -30,5 +31,17 @@ router.post("/admin/products/new", requireAuth, upload.single('image'), [require
 
     res.redirect('/admin/products');
 })
+
+// The : operator is a wildcard operator
+router.get('/admin/products/:id/edit', requireAuth, async (req, res) => {
+    const product = await productsRepo.getOne(req.params.id);
+
+    if(!product){
+        return res.send('Product not found');
+    }
+
+    res.send(productsEditTemplate({product}));
+});
+
 
 module.exports = router;
