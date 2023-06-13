965df8a4f3dc04f45f5b2ee4c3dd8f75b9ebdbc3
youzhenlim <limyouzhen@gmail.com>
2023-06-13 15:37:21 +0800

    add items to cart logic

--- repositories/carts.js
+++ repositories/carts.js
@@ -1,5 +1,7 @@
 const Repository = require('./repository');
 
-class CartsRepository extends Repository{}
+class CartsRepository extends Repository{
+
+}
 
 module.exports = new CartsRepository('carts.json');
--- routes/carts.js
+++ routes/carts.js
@@ -1,11 +1,38 @@
 const express = require('express');
-
+const cartsRepo = require('../repositories/carts');
 
 const router = express.Router();
 
 // Receive post request to add item to cart
-router.post('/cart/products', (req,res) => {
-    console.log(req.body.productId);
+router.post('/cart/products', async (req,res) => {
+    
+    let cart;
+
+    //Does user have existing cart?
+    if(!req.session.cartId){
+        cart = await cartsRepo.create({ items: [] });
+        req.session.cartId = cart.id;
+    }
+
+    else{
+        cart = await cartsRepo.getOne(req.session.cartId);
+    }
+
+    //Increment quantity or add new product to cart?
+    const existingItem = cart.items.find(item => item.id === req.body.productId);
+    
+    if(existingItem){
+        existingItem.quantity++;
+    }
+
+    else{
+        cart.items.push({id: req.body.productId, quantity: 1}); 
+    }
+
+    await cartsRepo.update(cart.id, {
+        items: cart.items
+    });
+
     res.send('Product added to cart');
 });
 // Receive get request to show all items in cart
