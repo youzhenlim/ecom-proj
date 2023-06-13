2857ebd616bcc3a5f4361adffb551d4a4171b193
youzhenlim <limyouzhen@gmail.com>
2023-06-13 13:07:59 +0800

    added simple render and retrieval of data

--- routes/products.js
+++ routes/products.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
+const productsRepo = require('../repositories/products');
+const productsIndexTemplate = require('../views/products/index');
 
 const router = express.Router();
 
 router.get('/', async(req, res) => {
-    res.send('Products here!');
+    const products = await productsRepo.getAll();
+    res.send(productsIndexTemplate({ products }));
 });
 
 
